9f07affcc0b2102bae7aa0f49a4077f289ab4f15
Eduardo E Ferrer M <ekferrerm@gmail.com>
2022-07-23 23:07:45 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import PlayerCounter from './lib/PlayerCounter.svelte'
+  const pageTitle = 'Svelte Player Counter'
+</script>
+<section>
+  <h1>{pageTitle}</h1>
+  <div class="wrapper">
+    <PlayerCounter playerName="Lennon"></PlayerCounter>
+    <PlayerCounter playerName="McCartney"></PlayerCounter>
+  </div>
+</section>
+
+<style>
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .wrapper {
+    display: flex;
+  }
+</style>
